d8c50fa5b83c410eb587a7751337edcfbe73cd33
Aebel Shajan <aebelshajan@gmail.com>
2023-12-11 21:05:28 +0000

    Finished adding calculator logic, project finished :D

--- src/App.js
+++ src/App.js
@@ -1,53 +1,176 @@
+import React, { useState } from "react";
 
-function Key({text, id, span=1, vSpan=1, handleClick}) {
+let output = [];
+const numberKeys = [
+	{ text: "1", id: "one" },
+	{ text: "2", id: "two" },
+	{ text: "3", id: "three" },
+	{ text: "4", id: "four" },
+	{ text: "5", id: "five" },
+	{ text: "6", id: "six" },
+	{ text: "7", id: "seven" },
+	{ text: "8", id: "eight" },
+	{ text: "9", id: "nine" },
+	{ text: "0", id: "zero", span: 2 },
+	{ text: ".", id: "decimal" }
+]
+const operations = [
+	{ text: "/", id: "divide" },
+	{ text: "x", id: "multiply" },
+	{ text: "-", id: "subtract" },
+	{ text: "+", id: "add" }
+]
+
+function Key({ text, id, span = 1, vSpan = 1, onClick }) {
 	const spanStyle = {
 		gridColumn: "span " + span,
 		gridRow: "span " + vSpan
 	}
 	return (
-			<button type="button" className={"button"} style={spanStyle} id={id}>
+		<button
+			type="button"
+			className={"button"}
+			id={id}
+			style={spanStyle}
+			onClick={() => onClick(text)}
+		>
 			{text}
 		</button>
 	)
 }
 
 function App() {
-	const numberKeys = [
-		{text: "1", id: "one"},
-		{text: "2", id: "two"},
-		{text: "3", id: "three"},
-		{text: "4", id: "four"},
-		{text: "5", id: "five"},
-		{text: "6", id: "six"},
-		{text: "7", id: "seven"},
-		{text: "8", id: "eight"},
-		{text: "9", id: "nine"},
-		{text: "0", id: "zero", span: 2},
-		{text: ".", id: "decimal"}
-	]
-	const operations = [
-		{text: "/", id: "divide"},
-		{text: "x", id: "multiply"},
-		{text: "-", id: "minus"},
-		{text: "+", id: "add"},
-		{text: "=", id: "equals", vSpan: 2}
-		
-	]
-	const displayKeys = (keys) => {
-		return keys.map(element => (<Key key={element.id} {...element}/>))
+	const [currentType, setCurrentType] = useState("number");
+	const [currentVal, setCurrentVal] = useState("0");
+	const [expression, setExpression] = useState("");
+	const [lastPressed, setLastPressed] = useState(null);
+
+	const updateExpression = () => {
+		setExpression(expression + currentVal);
+		setCurrentVal("");
 	}
-	
-  return (
-    <div id="calculator">
-			<div id="display">
+
+	const handleAC = () => {
+		setCurrentVal("0");
+		setExpression("");
+		setCurrentType("number");
+	};
+
+
+	const handleNumber = (number) => {
+		if (lastPressed === "=") {
+			setCurrentVal(prevVal => "")
+		}
+
+		if (number === ".") {
+			if (!currentVal.includes(".") && /^-?\d+/.test(currentVal)) {
+				setCurrentVal(currentVal => currentVal + ".");
+			}
+		} else {
+			if (currentVal === "0") {
+				setCurrentVal(number);
+			} else {
+				if (currentType === "operator") {
+					setCurrentType("number");
+					updateExpression();
+					setCurrentVal(number);
+				} else {
+					setCurrentVal(currentVal => currentVal + number);
+				}
+			}
+		}
+		setLastPressed(number);
+	};
+
+const handleOperation = (operator) => {
+		switch (currentType) {
+			case "number":
+				const enteringNewNumber = (expression === "" && currentVal === "0") || currentVal === "";
+				const alreadyEnteredNumber = /^[-+]?(\d+[.])?\d+/.test(currentVal);
+				const numberIsSign = currentVal === "+" || currentVal === "-";
+				switch (true) {
+					case enteringNewNumber:
+						console.log("1");
+						if (operator === "-" || operator === "+") {
+							setCurrentVal(operator);
+						}
+						break;
+					case numberIsSign:
+						console.log("2");
+						if (expression !== "") {
+							setExpression(expression => expression.slice(0, -1));
+							setCurrentType("operator");
+							setCurrentVal(operator);
+						}
+						break;
+					case alreadyEnteredNumber:
+						console.log("3");
+						setCurrentType("operator");
+						updateExpression();
+						setCurrentVal(operator);
+						break;
+					default:
+						break;
+				}
+				break;
+			case "operator":
+				if (operator === "-") {
+					updateExpression();
+					setCurrentType("number");
+					setCurrentVal("-");
+				} else {
+					setCurrentVal(operator);
+				}
+				break;
+			default:
+				break;
+		}
+		setLastPressed(operator);
+	};
+
+
+	const handleEquals = () => {
+		if (expression && currentVal) {
+			const newValue = evaluateExpression(expression + currentVal);
+			setCurrentVal(newValue);
+			output.push(expression + currentVal + "= " + newValue);
+			setExpression("");
+		}
+		setLastPressed("=");
+	};
+
+	const evaluateExpression = (expr) => {
+		let sanitizedExpr = expr.replace(/x/g, "*");
+		sanitizedExpr = sanitizedExpr.replace(/--/g, "+");
+		sanitizedExpr = sanitizedExpr.replace(/[+]-/g, "-");
+		try {
+			// eslint-disable-next-line no-eval
+			return eval(sanitizedExpr).toString();
+
+		} catch (error) {
+			return "Error";
+		}
+	};
+
+	const displayKeys = (keys, handleClick) => {
+		return keys.map(element => (<Key key={element.id} onClick={handleClick} {...element} />))
+	}
+
+	return (
+		<>
+			<div id="calculator">
+				<div id="display">
+					{expression + currentVal}
+				</div>
+				<Key text="AC" id="clear" span={2} onClick={handleAC} />
+				{displayKeys(operations, handleOperation)}
+				<Key text="=" id="equals" vSpan={2} onClick={handleEquals} />
+				<div id="numberKeys">
+					{displayKeys(numberKeys, handleNumber)}
+				</div>
 			</div>
-			<Key text="AC" id="AC" span={2} />
-			{displayKeys(operations)}
-			<div id="numberKeys">
-				{displayKeys(numberKeys)}
-			</div>
-    </div>
-  );
+		</>
+	);
 }
 
 export default App;
